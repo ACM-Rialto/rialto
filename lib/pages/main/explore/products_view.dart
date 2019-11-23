c7a494fbc83b89d0923fbfec5bd778a43207c50b
Arham 4 <arhamjsiddiqui@gmail.com>
2019-11-23 15:27:01 -0600

    Improved how the loading bar looks once image is loaded

--- lib/pages/main/explore/products_view.dart
+++ lib/pages/main/explore/products_view.dart
@@ -1,9 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:rialto/data/product.dart';
 import 'package:rialto/pages/main/item/interested_users_view.dart';
 import 'package:rialto/utils/text_utilities.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 class ProductsView extends StatefulWidget {
   final Firestore firestore = Firestore.instance;
@@ -67,116 +67,83 @@ class _SingleProductView extends StatelessWidget {
       padding: const EdgeInsets.only(left: 0.0, top: 10.0, bottom: 8.0),
       child: Card(
         elevation: 6.0,
-        color: Colors.cyanAccent,
-        child: Hero(
-          tag: _product.name,
-          child: Material(
-            child: InkWell(
-                onTap: () {},
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisSize: MainAxisSize.min,
-                  verticalDirection: VerticalDirection.down,
-                  children: <Widget>[
-                    Expanded(
-                      child: FittedBox(
-                        fit: BoxFit.contain, // otherwise the logo will be tiny
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Stack(
-                            children: <Widget>[
-                              Center(
-                                child: CircularProgressIndicator(
-                                  valueColor: new AlwaysStoppedAnimation(
-                                    Colors.redAccent,
-                                  ),
-                                ),
-                              ),
-                              Center(
-                                child: FadeInImage.memoryNetwork(
-                                  placeholder: kTransparentImage,
-                                  image: _product.image,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+        child: InkWell(
+            onTap: () {},
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              verticalDirection: VerticalDirection.down,
+              children: <Widget>[
+                Expanded(
+                  child: FittedBox(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new CachedNetworkImage(
+                        imageUrl: _product.image,
+                        placeholder: (context, url) =>
+                            CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                    Colors.redAccent)),
+                        errorWidget: (context, url, error) =>
+                            Image.asset("assets/images/logo.png"),
                       ),
                     ),
-                    Text(
-                      getTextWithCap(_product.name, 15),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    Text(
-                      "\$${_product.price}",
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold),
-                    ),
-                    FlatButton(
-                      color: Colors.redAccent,
-                      onPressed: () async {
-                        DocumentSnapshot snapshot = await Firestore.instance
-                            .collection('items')
-                            .document(_product.documentId)
-                            .get();
-                        // todo simplify this with helper class for firestore
-                        Map namesForEmail = snapshot.data['names_for_email'];
-                        if (_product.sellerEmail == "a@utdallas.edu") {
-                          showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  backgroundColor: Colors.white,
-                                  content: InterestedUsersView(snapshot),
-                                );
-                              });
-                        } else {
-                          namesForEmail['a@utdallas.edu'] = 'Arham Siddiqui';
-                          snapshot.reference.updateData({
-                            'names_for_email': namesForEmail,
+                  ),
+                ),
+                Text(
+                  getTextWithCap(_product.name, 15),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+                Text(
+                  "\$${_product.price}",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+                FlatButton(
+                  color: Colors.redAccent,
+                  onPressed: () async {
+                    DocumentSnapshot snapshot = await Firestore.instance
+                        .collection('items')
+                        .document(_product.documentId)
+                        .get();
+                    // todo simplify this with helper class for firestore
+                    Map namesForEmail = snapshot.data['names_for_email'];
+                    if (_product.sellerEmail == "a@utdallas.edu") {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              backgroundColor: Colors.white,
+                              content: InterestedUsersView(snapshot),
+                            );
                           });
-                          Scaffold.of(context).showSnackBar(
-                            new SnackBar(
-                              content: new Text(
-                                  "You have marked this item as interested!"),
-                            ),
-                          );
-                        }
-                      },
-                      child: Text(
-                        _product.sellerEmail == 'a@utdallas.edu'
-                            ? "View Interested"
-                            : "Interested",
-                        style: TextStyle(
-                          color: Colors.white,
+                    } else {
+                      namesForEmail['a@utdallas.edu'] = 'Arham Siddiqui';
+                      snapshot.reference.updateData({
+                        'names_for_email': namesForEmail,
+                      });
+                      Scaffold.of(context).showSnackBar(
+                        new SnackBar(
+                          content: new Text(
+                              "You have marked this item as interested!"),
                         ),
-                      ),
-                    )
-                  ],
+                      );
+                    }
+                  },
+                  child: Text(
+                    _product.sellerEmail == 'a@utdallas.edu'
+                        ? "View Interested"
+                        : "Interested",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 )
-//          child: GridTile(
-//            footer: Container(
-//              color: Colors.white70,
-//              child: ListTile(
-//                leading: Text(prodName, style:TextStyle(fontWeight: FontWeight.bold),
-//                ),
-//                title: Text("\$$prodPrice", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16.0),
-//                ),
-//              ),
-//            ),
-//
-//            child: Padding(
-//              padding: const EdgeInsets.all(8.0),
-//              child: Image.asset(prodImage, fit:BoxFit.cover),
-//            ),
-//          ),
-            ),
-          ),
-        ),
+              ],
+            )),
       ),
     );
   }
