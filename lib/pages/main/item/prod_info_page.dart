a23539e20f58905d5fd449e6172b23f496e1a3ea
jibib <42705262+jibib@users.noreply.github.com>
2019-11-30 16:43:49 -0600

    Add files via upload

---
+++ lib/pages/main/item/prod_info_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:rialto/data/product.dart';
+
+
+/*
+class ProductInfo extends StatefulWidget {
+  @override
+
+  State<StatefulWidget> createState() {
+    return new ProductInfoState();
+  }
+}
+*/
+
+class ProductInfo extends StatelessWidget {
+
+final Product prod;
+
+ProductInfo({this.prod});
+
+  @override
+Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text("PRODUCT INFORMATION"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(12.0),
+        alignment: Alignment.centerLeft,
+        child: ListView(
+          children: <Widget>[
+            Image.network(prod.image,
+                      height: 250,
+                      width: 400,),
+            
+                    Text(
+                      "${prod.name}",
+                      style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black87,
+                    ),
+                    textAlign: TextAlign.center,
+            ),
+            
+            SizedBox(height : 25),
+
+            new Row(
+              
+              children: <Widget>[
+              
+              Text(
+                      "Price: \$${prod.price}",
+                      style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.redAccent,
+                      fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+              SizedBox(width: 125),
+
+              new FlatButton(
+                color: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "Contact",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.redAccent
+                          ),
+                        ),
+              ),
+
+              ],),
+            
+
+            
+
+            new Card(
+              child: new Container(
+                padding: new EdgeInsets.all(32.0),
+                child: new Column(
+                  children: <Widget>[
+                    Text(
+                      "Description: ${prod.description}",
+                      style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      ),
+                    ),
+                  ],)
+              )
+            ),
+          ],),
+        )
+    );
+  }
+}
